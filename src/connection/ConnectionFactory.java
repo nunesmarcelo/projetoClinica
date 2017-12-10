35878901676708dc9ad9b39ec4190e173854ee8e
Marcelo Nunes <marcelonunes3698@gmail.com>
2017-12-10 11:42:34 -0200

    BUG DO BD

--- src/connection/ConnectionFactory.java
+++ src/connection/ConnectionFactory.java
@@ -6,6 +6,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import javax.swing.JOptionPane;
+
 public class ConnectionFactory {
     private static final String DRIVER = "com.mysql.jdbc.Driver";
     private static final String URL = "jdbc:mysql://localhost:3306/clinica";
@@ -18,6 +20,16 @@ public class ConnectionFactory {
             
              return DriverManager.getConnection(URL, USER, PASS);
         } catch (ClassNotFoundException | SQLException ex) {
+        	JOptionPane.showMessageDialog(null, "Houve um erro ao conectar ao banco de dados!\n "
+        			+ "PossÌveis problemas: \n "
+        			+ "1∫ -> Seu XAMPP, LAMP ou WAMP podem estar desligados.\n"
+        			+ "2∫ -> Se tudo estiver bem, talvez a importaÁ„o da biblioteca de conex„o "
+        			+ "n„o foi feita corretamente.\n"
+        			+ "		2.1 -> Clique com o bot„o direito no projeto, e depois em 'Build Path' e"
+        			+ " v· em 'Add External Archives'.\n"
+        			+ "		2.2 -> Caminhe atÈ a pasta onde o projeto est· instalado, v· na pasta 'lib'"
+        			+ " e selecione todos os arquivos de l·.\n"
+        			+ "		2.3 -> rode novamente o programa. =) ");
             throw new RuntimeException("Erro na conex√£o!",ex);
         }
     }
